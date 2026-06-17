ce59c09f293eed4eea0277a59b29e5539275a2f7
Albedo-AB <di.aravenab@duocuc.cl>
2026-06-17 09:48:17 -0400

    Agrega geteway a mc- Auth, User y Catalog

---
+++ mc-api-getway-garonpro/src/main/java/com/example/mc_api_getway_garonpro/McApiGetwayGaronproApplication.java
@@ -0,0 +1,13 @@
+package com.example.mc_api_getway_garonpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McApiGetwayGaronproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McApiGetwayGaronproApplication.class, args);
+	}
+
+}
---
+++ mc-api-getway-garonpro/src/test/java/com/example/mc_api_getway_garonpro/McApiGetwayGaronproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mc_api_getway_garonpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McApiGetwayGaronproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
